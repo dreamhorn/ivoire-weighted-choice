365cf5684a5be8f87da1bad1fd430173820132ea
eykd <david.eyk@gmail.com>
2015-07-08 21:05:46 -0700

    Update tests.

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
 var chai = require('chai');
-var Ivoire = require('ivoire-weighted-choice');
+var Ivoire = require('./lib/ivoire-weighted-choice');
 
 chai.should();
 
 describe('ivoire-weighted-choice', function () {
   var seed = 42
-  var i;
+  var ivoire;
 
   beforeEach(function(){
-    i = new Ivoire({seed: seed});
+    ivoire = new Ivoire({seed: seed});
   });
 
   describe('#weighted_choice()', function () {
     it('should make a weighted choice', function () {
-      i.weighted_choice(['foo', 'bar'], [10, 1]).should.equal('foo')
+      ivoire.weighted_choice(['foo', 'bar'], [10, 1]).should.equal('foo')
     });
   });
 });
